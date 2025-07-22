c2dc3f581ce0907d50b8bd2c80b13fc85c08f4d7
Sindhuu001 <sindhuredde07@example.com>
2025-07-22 12:02:05 +0530

    initial commit

---
+++ src/main/java/com/example/ProjectManagementModule/ProjectManagementModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectManagementModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementModuleApplication.class, args);
+	}
+
+}
